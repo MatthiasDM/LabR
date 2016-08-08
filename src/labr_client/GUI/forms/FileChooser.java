4f1424f1f71a24b543dcfe931823607abc09d00c
popo <popo@192.168.0.212>
2016-08-08 14:41:35 +0200

    License and author change

--- src/labr_client/GUI/forms/FileChooser.java
+++ src/labr_client/GUI/forms/FileChooser.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2016 De Mey Matthias
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package labr_client.GUI.forms;
 
 import java.io.File;
@@ -7,7 +23,7 @@ import javax.swing.JFileChooser;
 
 /**
  *
- * @author labbl
+ * @author De Mey Matthias
  */
 public class FileChooser extends javax.swing.JFrame {
 
